e3e2ecd8647d6cf01775e503464c8a5b7c2cdea2
sonho463 <son.ho463@gmail.com>
2020-12-29 12:16:31 +0900

    配列セットして表示までOK

--- scripts/main.js
+++ scripts/main.js
@@ -25,3 +25,64 @@ function startPomo(){
 		return timeStr.getTime() + min * 1000 * 60
 	}
 }
+
+function makePomoArray(startTime){
+
+	let time_arr = []; // 配列定義
+	console.log(startTime);
+	const TO_MINUTES = 1000 * 60; // ミリ秒を分に変換定数
+
+	let startTime_msec = startTime.getTime();　// 時刻をミリ秒に変換
+	time_arr.push(startTime_msec); // スタート時刻プッシュ
+
+	// ２５−５のサイクルを３回配列にプッシュ
+	for(let i = 1; i<8; i++){
+		i % 2 != 0 ?
+			startTime_msec = startTime_msec + 25 * TO_MINUTES:
+			startTime_msec = startTime_msec + 5 * TO_MINUTES ;
+		time_arr.push(startTime_msec);
+	};
+
+	// 長めの休み
+	let long_break = startTime_msec + 15 * TO_MINUTES;
+	time_arr.push(long_break);
+
+	// mapで時刻表示にして配列にセット
+	let arr = [];
+	arr = time_arr.map(time => {
+		let date = new Date(time);
+		let hh = date.getHours();
+		let mm = date.getMinutes();
+		mm < 10 ? mm = '0' + date.getMinutes() : mm;
+		return `${hh}:${mm}`;
+	});
+	return arr;
+	}
+
+
+function addElement () {
+	rmSchedule(); //今の配列を削除
+	arr = makePomoArray(new Date());
+	arr.forEach(function(a){
+		// 新しい要素を作成します
+		let pomoSchedule = document.createElement("h2");
+		//　１つずつの要素をnewContentに格納
+		let newContent = document.createTextNode(a);
+		// 挿入する要素の参照を取得
+		let Parent = document.getElementById("schedule");
+		// テキストノードを新規作成した h2 に追加します
+		pomoSchedule.appendChild(newContent);
+		// DOM に新しく作られた要素とその内容を追加します
+		let currentDiv = document.getElementById("div1");
+		Parent.insertBefore(pomoSchedule, currentDiv);
+	});
+}
+
+function rmSchedule () {
+	let schedule = document.querySelector('#schedule');
+	let h2 = schedule.querySelectorAll('h2');
+
+	h2.forEach(function(h){
+		schedule.removeChild(h);
+	})
+}
